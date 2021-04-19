27749cd024df25dac684d0106ec85fc73733ca3c
Fatih Yavaşoğlu <fatihyavasoglu@gmail.com>
2021-04-19 18:30:40 +0300

    düzenleme

--- zepllin/Database/DatabaseContext.cs
+++ zepllin/Database/DatabaseContext.cs
@@ -42,7 +42,7 @@ namespace zepllin.Database
         {
             get
             {
-                return "server=localhost;uid=root;password=951753;database=zepllin;Allow User Variables=True";
+                return "server=database;uid=root;password=123456;database=databasename;Allow User Variables=True";
             }
         }
         #endregion
